9ca6fa44eb689736f9b869622cc46cb6f793f7ab
elspethlawson <lawsonelspeth@gmail.com>
2016-07-24 17:00:24 +0100

    Created router using flask in python

---
+++ router.py
@@ -0,0 +1,23 @@
+from flask import Flask 
+from flask import render_template
+
+app = Flask("MyApp")
+
+@app.route("/")
+def home():
+	return render_template ("home.html")
+
+@app.route("/breakfast")
+def breakfast():
+	return render_template ("breakfast.html")
+
+
+@app.route("/lunch")
+def lunch():
+	return render_template("lunch.html")
+
+@app.route("/dinner")
+def dinner():
+	return render_template("dinner.html")
+
+app.run()
